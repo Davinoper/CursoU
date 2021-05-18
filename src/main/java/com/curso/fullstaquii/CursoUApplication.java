8d94433e7ef1e50f6300dde547abe1fb38d64181
Davinoper <davidnpjs@gmail.com>
2021-05-18 18:26:01 -0300

    primeira versao

---
+++ src/main/java/com/curso/fullstaquii/CursoUApplication.java
@@ -0,0 +1,13 @@
+package com.curso.fullstaquii;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CursoUApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CursoUApplication.class, args);
+	}
+
+}
